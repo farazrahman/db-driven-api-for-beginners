04f9b8414ae7fbf59bb179a5406d311d14acfe3c
farazrahman <faraz.rahman41@gmail.com>
2022-02-13 00:07:12 -0800

    add mapper dictionaries

---
+++ data_ingestion_ETL/classification_mapper.py
@@ -0,0 +1,20 @@
+
+# Mapping dictionary for sequencing strategy
+sequencing_strategy_mapper = {'Whole Genome Sequencing': 1, 'Metatranscriptome': 2}
+
+# Mapping dictionary for sequencing status
+sequencing_status_mapper = {'Complete': 1, 'Incomplete': 2}
+
+# Mapping dictionary for sequencing centers
+sequencing_center_mapper = {'DOE Joint Genome Institute (JGI)': 1, 'Max Planck Institute': 2}
+
+# Mapping dictionary for project status
+project_status_mapper = {'Complete and Published': 1, 'Permanent Draft': 2}
+
+# Mapping dictionary for project funding
+project_funding_mapper = {'U.S. Department of Energy (DOE)': 1, 'Max Planck Institute': 2}
+
+# Mapping dictionary for project funding
+analysis_type_mapper = {'Metatranscriptome Analysis': 1, 'Genome Analysis (Isolate)': 2, 'Metatranscriptome mapping (other)': 3, 'Metatranscriptome mapping (self)': 4}
+
+
