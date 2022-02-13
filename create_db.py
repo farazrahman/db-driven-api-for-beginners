ecaf268f3b4e6aeba9e66c2e0b2e994c38e8139c
farazrahman <faraz.rahman41@gmail.com>
2022-02-12 17:43:00 -0800

    update docstring

--- create_db.py
+++ create_db.py
@@ -7,7 +7,7 @@ DB_NAME = "interview.db"
 
 # The below code tries to break the Ex_sp_data into 5 different tables like by project_gold_id:
 # sequence table- contains details of sequencing strategy, sequencing status and centers for each project id
-sequence_table = """CREATE TABLE IF NOT EXISTS sequence ([project_gold_id] VARCHAR(10) PRIMARY KEY NOT NULL,
+sequence_table = """CREATE TABLE IF NOT EXISTS sequence ([project_gold_id] VARCHAR PRIMARY KEY NOT NULL,
                                                         [sequencing_strategy] VARCHAR NULL,
                                                         [sequencing_status] VARCHAR NULL,
                                                         [sequencing_centers] VARCHAR NULL);"""
@@ -55,7 +55,7 @@ tables_lst = [project_table, sequence_table, study_table, ncbi_table, analysis_t
 def create_tables(list_of_tables: List[str]):
     """
 
-    :param list_of_tables: list of tables created with SQL command to create a table in the database
+    :param list_of_tables: list of tables created with SQL command
     """
     sqlite_connection = sqlite3.connect(DB_NAME)
     cursor = sqlite_connection.cursor()
@@ -71,12 +71,12 @@ if __name__ == '__main__':
     # # first drop tables if required else keep it commented
     # sqlite_connection = sqlite3.connect(DB_NAME)
     # cursor = sqlite_connection.cursor()
-    # cursor.execute('''DROP TABLE analysis;''')
-    # cursor.execute('''DROP TABLE ncbi;''')
-    # cursor.execute('''DROP TABLE projects;''')
-    # cursor.execute('''DROP TABLE sequence;''')
-    # cursor.execute('''DROP TABLE studies;''')
-    # cursor.execute('''DROP VIEW sp_ap_table;''')
+    # cursor.execute('''DROP TABLE IF EXISTS analysis;''')
+    # cursor.execute('''DROP TABLE IF EXISTS ncbi;''')
+    # cursor.execute('''DROP TABLE IF EXISTS projects;''')
+    # cursor.execute('''DROP TABLE IF EXISTS sequence;''')
+    # cursor.execute('''DROP TABLE IF EXISTS studies;''')
+    # cursor.execute('''DROP VIEW IF EXISTS sp_ap_table;''')
     # sqlite_connection.close()
 
     # create tables in the db
